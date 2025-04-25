8b717c565802d624efa386366a98b12ec9515e51
Santiago-909 <smeza4@ucsc.edu>
2025-04-24 17:44:10 -0700

    lab6

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6 javascript
+// Author: Santiago Meza
+// Date: 4-24-2025
 
 // Constants
 
@@ -19,3 +19,26 @@ function main() {
 
 // let's get this party started
 main();
+
+
+// Define Variables
+myTransport = ["car", "bus", "walking", "bike", "run"]; 
+
+//create obeject for my main ride 
+myMainRide = {
+  make: "Volkswagen", 
+  model: "Jetta", 
+  color: "Gray", 
+  year: 2006, 
+  // here's our function
+  age: function () {
+      return 2025 - this.year; 
+  }
+} 
+
+
+// output
+document.writeln("Kinds of Transportation I use: ", myTransport, "<br>");
+//cool trick to let us write into the document  
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>"); 
