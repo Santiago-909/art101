a7bbfa2db7efa8b6ffc51850882c75c9c22e47d0
Santiago-909 <smeza4@ucsc.edu>
2025-05-26 13:25:44 -0700

    test

---
+++ lab12/js/lab.js
@@ -0,0 +1,30 @@
+// index.js - Lab 9 
+// Author: Santiago Meza
+// Date: 5-13-2025
+
+
+
+
+
+
+
+
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+
+
+
+
+
+// let's get this party started
+main();
