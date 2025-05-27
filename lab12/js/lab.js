6bbb2ad08677fbc2f1cbd412de75bf7c65602f27
Santiago-909 <smeza4@ucsc.edu>
2025-05-27 12:06:27 -0700

    test

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -2,6 +2,31 @@
 // Author: Santiago Meza
 // Date: 5-13-2025
 
+for (i=0; i<=200;i++){ 
+  let str = ""; 
+  if (i % 3 ==0){
+    str +="Fizz";
+  }
+   if (i % 5 ==0){
+    str +="Buzz";
+  }
+   if (i % 7 ==0){
+    str +="Boom";
+  }
+  if (!str){ 
+  str = i;
+  }
+  else {
+    str +="!";
+  }
+  console.log(str);
+ $("#output").append("<p class='num'>" + str + "</p>");
+}
+
+
+				
+			
+
 
 
 
